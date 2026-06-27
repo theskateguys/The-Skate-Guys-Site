20e6b8abdadbf31230ccb98e0a40a4b9b165868a
The Skate Guys <info.theskateguys@gmail.com>
2026-06-27 12:08:45 -0400

    Render events from shared data file

---
+++ assets/events/events-data.js
@@ -0,0 +1,97 @@
+// Edit this file for event updates. Keep active/upcoming events in the order
+// you want them to appear on the site.
+window.TSG_EVENTS = [
+  {
+    id: "lambeau-family-day-2026",
+    title: "2nd Annual Lambeau Day",
+    shortTitle: "Lambeau Family Day",
+    dateLabel: "Saturday 20 June 2026",
+    timeLabel: "10:00 AM",
+    regionLabel: "Tobago",
+    locationLabel: "Lambeau Recreational Grounds",
+    description: "Harvest Gawd presents a full family day at Lambeau Recreational Grounds, and The Skate Guys Community will be there with free roller skates.",
+    image: "assets/events/lambeau-day.jpg.jpeg",
+    imageAlt: "Lambeau Family Day flyer",
+    theme: "pink",
+    marker: "TSG",
+    tags: [
+      "10:00 AM",
+      "Lambeau Rec Grounds",
+      "Free roller skates",
+      "5 vs 5 football",
+      "Sack race, tug-of-war and face painting"
+    ],
+    callout: "The Skate Guys Community will be there. Bring your family and roll with us.",
+    ctaLabel: "Find Out More",
+    whatsappText: "Hi TSG! I'd like to know more about Lambeau Family Day on June 20.",
+    tickerText: "Sat 20 June - Lambeau Recreational Grounds, Tobago - Free roller skates - Skate Guys will be there!",
+    schema: {
+      startDate: "2026-06-20T10:00:00-04:00",
+      endDate: "2026-06-20T18:00:00-04:00",
+      placeName: "Lambeau Recreational Grounds",
+      addressLocality: "Lambeau",
+      addressRegion: "Tobago",
+      addressCountry: "TT",
+      offers: [
+        {
+          name: "Community Activity",
+          price: "0",
+          priceCurrency: "TTD"
+        }
+      ]
+    }
+  },
+  {
+    id: "lowlands-popup-skate-class-2026",
+    title: "Pop-Up Skate Class",
+    shortTitle: "Pop-Up Skate Class",
+    dateLabel: "Jun 26, 27 and 28",
+    timeLabel: "3:00 - 6:00 PM",
+    regionLabel: "Tobago",
+    locationLabel: "Lowlands Mall, in front of Movie Towne",
+    description: "Quick tutorial sessions for adults and kids at Lowlands Mall. Three days, all skill levels, rental skates available.",
+    image: "assets/events/popup-lowlands.jpg.jpg",
+    imageAlt: "Pop-Up Skate Class Lowlands Mall flyer",
+    theme: "cyan",
+    marker: "POP",
+    tags: [
+      "Jun 26, 27 and 28",
+      "3:00 - 6:00 PM",
+      "Lowlands Mall, Tobago",
+      "1 free water per student"
+    ],
+    priceCards: [
+      {
+        amount: "$50",
+        label: "TTD with rental skates"
+      },
+      {
+        amount: "$40",
+        label: "TTD with own skates"
+      }
+    ],
+    ctaLabel: "Book via WhatsApp",
+    whatsappText: "Hi TSG! I'd like to book a spot at the Lowlands Mall Pop-Up Skate Class.",
+    tickerText: "Jun 26, 27 and 28 - Lowlands Mall, Tobago - $50 TTD with rentals - $40 TTD own skates - 3-6 PM daily",
+    schema: {
+      startDate: "2026-06-26T15:00:00-04:00",
+      endDate: "2026-06-28T18:00:00-04:00",
+      placeName: "Lowlands Mall (In front of Movie Towne)",
+      addressLocality: "Lowlands",
+      addressRegion: "Tobago",
+      addressCountry: "TT",
+      offers: [
+        {
+          name: "With Rental Skates",
+          price: "50",
+          priceCurrency: "TTD"
+        },
+        {
+          name: "With Own Skates",
+          price: "40",
+          priceCurrency: "TTD"
+        }
+      ]
+    }
+  }
+];
